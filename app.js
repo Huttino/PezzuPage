2ca7ab5bb8cf8f5338dbcaa0582f4a5d297ab5aa
Huttino <107991916+Huttino@users.noreply.github.com>
2022-11-22 14:45:03 +0100

    quasi meglio

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("show");
-    } else {
-      entry.target.classList.remove("show");
-    }
-  });
-});
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting && !entry.target.classList.contains("show")) {
+        entry.target.classList.add("show");
+      } else if (
+        !entry.isIntersecting &&
+        entry.target.classList.contains("show")
+      ) {
+        entry.target.classList.remove("show");
+      }
+    });
+  },
+  { rootMargin: "0px", threshold: 0.9 }
+);
 
 const hiddenElemnts = document.querySelectorAll(".hidden");
 hiddenElemnts.forEach((el) => observer.observe(el));
